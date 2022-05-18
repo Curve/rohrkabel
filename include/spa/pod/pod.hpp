7950e45c0557486ebdfc1113d0a8d0b6f69e9dd4
Curve <fynnbwdt@gmail.com>
2022-05-18 13:22:25 +0200

    feat: add spa_pod implementation

---
+++ include/spa/pod/pod.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include <string>
+#include <memory>
+#include <cstdint>
+
+struct spa_pod;
+struct spa_type_info;
+namespace pipewire::spa
+{
+    enum class pod_type
+    {
+        boolean = 2,
+        object = 15,
+    };
+
+    class pod_object_body;
+    class pod
+    {
+        struct impl;
+
+      private:
+        std::unique_ptr<impl> m_impl;
+
+      public:
+        ~pod();
+
+      public:
+        pod(spa_pod *);
+        pod(const spa_pod *);
+
+      public:
+        pod(pod &&) noexcept;
+
+      public:
+        pod &operator=(pod &&) noexcept;
+
+      public:
+        pod_type type() const;
+        std::size_t size() const;
+        std::string name() const;
+
+      public:
+        template <typename T> T &as() const = delete;
+        template <typename T> T body() const = delete;
+
+      public:
+        spa_pod *get() const;
+        const spa_type_info *get_type() const;
+    };
+
+    template <> bool &pod::as<bool>() const;
+    template <> pod_object_body pod::body<pod_object_body>() const;
+} // namespace pipewire::spa
---
+++ src/spa.pod.cpp
@@ -0,0 +1,77 @@
+#include "spa/pod/pod.hpp"
+#include "spa/pod/object/body.hpp"
+
+#include <spa/debug/pod.h>
+#include <spa/pod/builder.h>
+
+namespace pipewire::spa
+{
+    struct pod::impl
+    {
+        spa_pod *pod;
+        bool is_copy{false};
+        const spa_type_info *type;
+    };
+
+    pod::~pod()
+    {
+        if (m_impl && m_impl->is_copy)
+        {
+            free(m_impl->pod);
+        }
+    }
+
+    pod::pod(spa_pod *pod) : m_impl(std::make_unique<impl>())
+    {
+        m_impl->pod = pod;
+        m_impl->type = spa_debug_type_find(nullptr, pod->type);
+    }
+
+    pod::pod(const spa_pod *pod) : spa::pod(spa_pod_copy(pod))
+    {
+        m_impl->is_copy = true;
+    }
+
+    pod::pod(pod &&pod) noexcept : m_impl(std::move(pod.m_impl)) {}
+
+    pod &pod::operator=(pod &&pod) noexcept
+    {
+        m_impl = std::move(pod.m_impl);
+        return *this;
+    }
+
+    pod_type pod::type() const
+    {
+        return static_cast<pod_type>(m_impl->pod->type);
+    }
+
+    std::size_t pod::size() const
+    {
+        return m_impl->pod->size;
+    }
+
+    std::string pod::name() const
+    {
+        return m_impl->type->name;
+    }
+
+    template <> bool &pod::as<bool>() const
+    {
+        return reinterpret_cast<bool &>(reinterpret_cast<spa_pod_bool *>(m_impl->pod)->value);
+    }
+
+    template <> pod_object_body pod::body<pod_object_body>() const
+    {
+        return {*this};
+    }
+
+    spa_pod *pod::get() const
+    {
+        return m_impl->pod;
+    }
+
+    const spa_type_info *pod::get_type() const
+    {
+        return m_impl->type;
+    }
+} // namespace pipewire::spa
