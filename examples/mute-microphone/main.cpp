65866e92f34c35a3aff487f1fcf2e7e78623ce26
Curve <fynnbwdt@gmail.com>
2022-05-18 14:09:29 +0200

    feat: add mute-microphone example

---
+++ examples/mute-microphone/main.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+
+#include "registry/registry.hpp"
+#include "spa/pod/object/body.hpp"
+
+void search_mute_prop(pipewire::spa::pod_prop *&result, const pipewire::spa::pod &pod, pipewire::spa::pod_prop *parent_prop = nullptr);
+
+int main()
+{
+    auto main_loop = pipewire::main_loop();
+    auto context = pipewire::context(main_loop);
+    auto core = pipewire::core(context);
+    auto reg = pipewire::registry(core);
+
+    std::vector<pipewire::device> devices;
+
+    auto reg_listener = reg.listen<pipewire::registry_listener>();
+    reg_listener.on<pipewire::registry_event::global>([&](const pipewire::global &global) {
+        if (global.type == pipewire::device::type)
+        {
+            auto device = reg.bind<pipewire::device>(global);
+            auto info = device.info();
+
+            if (info.props.count("alsa.card_name"))
+            {
+                devices.emplace_back(std::move(device));
+            }
+        }
+    });
+    core.sync();
+
+    for (auto i = 0u; devices.size() > i; i++)
+    {
+        auto &device = devices.at(i);
+        auto name = device.info().props.at("alsa.card_name");
+
+        std::cout << i << ": " << name << std::endl;
+    }
+
+    std::cout << std::endl;
+    std::cout << "Select a device to mute: ";
+
+    std::size_t selection{0};
+    std::cin >> selection;
+
+    auto &device = devices.at(selection);
+
+    std::cout << std::endl;
+    std::cout << "Muting: " << device.info().props.at("alsa.card_name") << std::endl;
+
+    for (const auto &[pod_id, pod] : device.params())
+    {
+        pipewire::spa::pod_prop *result{nullptr};
+        search_mute_prop(result, pod);
+
+        if (result)
+        {
+            std::cout << "Mute-Prop: " << result->name() << " (" << pod_id << ") [" << result->key() << "]" << std::endl;
+            result->value().as<bool>() = true;
+
+            device.set_param(pod_id, pod.get());
+            core.sync();
+
+            std::cout << "Device muted!" << std::endl;
+            return 0;
+        }
+    }
+
+    std::cout << "Could not find mute prop for device!" << std::endl;
+    return 1;
+
+    //? Instead of enumerating all pods you could also use the short version:
+    /*
+        auto mute = pods.at(13).body<pipewire::spa::pod_object_body>().at(10).value().body<pipewire::spa::pod_object_body>().at(65540).value();
+
+        mute.as<bool>() = !mute.as<bool>();
+        device.set_param(13, pods.at(13).get());
+
+        core.sync();
+    */
+}
+
+// NOLINTNEXTLINE
+void search_mute_prop(pipewire::spa::pod_prop *&result, const pipewire::spa::pod &pod, pipewire::spa::pod_prop *parent_prop)
+{
+    if (pod.type() == pipewire::spa::pod_type::object)
+    {
+        for (auto prop : pod.body<pipewire::spa::pod_object_body>())
+        {
+            search_mute_prop(result, prop.value(), &prop);
+        }
+    }
+    if (pod.type() == pipewire::spa::pod_type::boolean)
+    {
+        if (parent_prop && parent_prop->name().find("mute") != std::string::npos)
+        {
+            result = new pipewire::spa::pod_prop(std::move(*parent_prop));
+        }
+    }
+}
