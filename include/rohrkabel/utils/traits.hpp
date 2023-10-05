efcd1ac68ded144ba4911a53dbfa42413856a633
Curve <fynnbwdt@gmail.com>
2023-10-05 09:37:46 +0200

    feat: add traits

---
+++ include/rohrkabel/utils/traits.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include "lazy.hpp"
+#include "../proxy.hpp"
+
+#include <cstdint>
+#include <concepts>
+#include <type_traits>
+
+namespace pipewire
+{
+    template <typename T>
+    concept is_pointer = std::is_pointer_v<T>;
+
+    template <typename T>
+    concept valid_proxy = requires(T obj) {
+        requires std::derived_from<T, proxy>;
+        requires std::is_nothrow_move_assignable_v<T>;
+        requires std::is_nothrow_move_constructible_v<T>;
+        {
+            obj.get()
+        } -> is_pointer;
+        {
+            obj.bind(nullptr)
+        } -> std::same_as<lazy<expected<T>>>;
+        {
+            obj.type
+        } -> std::same_as<const char *&>;
+        {
+            obj.version
+        } -> std::same_as<const std::uint32_t &>;
+    };
+} // namespace pipewire
