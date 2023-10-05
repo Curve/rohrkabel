19f2980fe3d83b3896b06a747a7a4873dc0e61c0
Curve <fynnbwdt@gmail.com>
2023-10-05 09:37:35 +0200

    feat: explicit lazy type

---
+++ include/rohrkabel/utils/lazy.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "../error.hpp"
+
+#include <future>
+#include <tl/expected.hpp>
+
+namespace pipewire
+{
+    template <typename T>
+    using expected = tl::expected<T, error>;
+
+    template <typename T>
+    using lazy = std::future<T>;
+
+    template <typename T, typename Function>
+        requires std::same_as<std::invoke_result_t<Function>, T>
+    lazy<T> make_lazy(Function &&);
+} // namespace pipewire
+
+#include "lazy.inl"
