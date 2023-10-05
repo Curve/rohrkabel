eba670a5b10f3c09e7997ef1ba46d184107ff9d1
Curve <fynnbwdt@gmail.com>
2023-10-05 09:38:04 +0200

    refactor: error as simple struct

--- include/rohrkabel/error.hpp
+++ include/rohrkabel/error.hpp
@@ -1,24 +1,10 @@
 #pragma once
-#include <string>
-#include <future>
-#include <stdexcept>
-#include <tl/expected.hpp>
-
 namespace pipewire
 {
-    class error : std::exception
+    struct error
     {
-        int m_seq, m_res;
-        const char *m_message;
-
-      public:
-        error(int seq, int res, const char *message);
-
-      public:
-        int seq() const noexcept;
-        int res() const noexcept;
-        const char *what() const noexcept override;
+        int seq;
+        int res;
+        const char *message;
     };
-
-    template <typename T> using lazy_expected = std::future<tl::expected<T, error>>;
 } // namespace pipewire
